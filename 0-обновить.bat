e4631d406d2d2c4d1605a7a5ccfcda4d85f1e85d
Andrii Prykhodko <a.v.prykhodko25@gmail.com>
2025-07-13 18:25:09 +0200

    feat: rework initialization

--- 0-обновить.bat
+++ 0-обновить.bat
@@ -1,21 +1,25 @@
 @echo off
 cd /d %~dp0
 
-REM Check if this folder is already a Git repository
 if not exist ".git" (
     echo No git repository found. Initializing...
     git init
 
-    echo Adding remote origin...
+    echo Adding remote...
     git remote add origin https://github.com/am-kenny/kayala-forms-masterok
+    
+    echo Fetching...
+    git fetch
 
-    echo Fetching remote master branch...
-    git fetch origin master
+    echo Setting branch...
+    git branch master origin/master
 
-    echo Checking out master branch from origin...
-    git checkout -b master origin/master
+    echo Hard-resetting branch...
+    git reset --hard origin/master
+) else (
+    echo Pulling latest changes...
+    git fetch
+    git reset --hard origin/master
 )
 
-echo Pulling latest changes...
-git pull
 pause
